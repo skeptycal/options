2b23e16d18624e2b5d408287bda49c54c7a010ec
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-22 09:39:06 -0500

    GoBot: setup repo files from template

---
+++ options.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package options contains utilities for macOS.
+package options
+
+import "fmt"
+
+func Example() {
+
+	fmt.Println("Example import acknowledgement from package options")
+}
