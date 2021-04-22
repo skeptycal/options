077a3e36ab2f5c6d92831c6329c483da06624be3
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-22 09:39:15 -0500

    (GoBot) devtag version : GoBot: dev branch and directory tree setup

---
+++ .github/.github.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package .github contains utilities for macOS.
+package .github
+
+import "fmt"
+
+func Example() {
+
+	fmt.Println("Example import acknowledgement from package .github")
+}
