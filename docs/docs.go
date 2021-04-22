77a23feb0417d67904ac96b2adcd4533910a403e
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-22 09:39:10 -0500

    GoBot: create GitHub Pages folder branch

---
+++ docs/docs.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package docs contains utilities for macOS.
+package docs
+
+import "fmt"
+
+func Example() {
+
+	fmt.Println("Example import acknowledgement from package docs")
+}
