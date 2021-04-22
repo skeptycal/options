bd5f72210f8724af752eaa8697aaedb1cff26b49
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-22 09:39:05 -0500

    Initial commit

---
+++ gorepotemplate.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package gorepotemplate contains utilities for macOS.
+package gorepotemplate
+
+import "fmt"
+
+func Example() {
+
+	fmt.Println("Example import acknowledgement from package gorepotemplate")
+}
